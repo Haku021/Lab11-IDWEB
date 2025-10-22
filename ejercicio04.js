31b87d15903835e86572798b246875cec65db5e1
Haku021 <jgonzalera@unsa.edu.pe>
2025-10-22 12:18:32 -0500

    cuarto ejercicio

---
+++ ejercicio04.js
@@ -0,0 +1,6 @@
+let número=parseInt(prompt("Ingrese un número: "));
+for(let i =0; i<12;i++){
+    let producto = número*(i+1);
+    console.log(número+" x "+(i+1)+"= "+producto);
+    alert(número+" x "+(i+1)+"= "+producto);
+}
