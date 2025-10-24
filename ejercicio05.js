b988e7c03ffa6fcf5116cefb7cdeedbaedbf0521
Haku021 <jgonzalesra@unsa.edu.pe>
2025-10-24 12:57:31 -0500

    ejercicio 5 completo

--- ejercicio05.js
+++ ejercicio05.js
@@ -4,17 +4,26 @@ let billetes50=0;
 let billetes20=0;
 let billetes10=0;
 
-while(monto)
-    if(monto>=100){
-        billetes100+=(monto-(monto%100))/100;
-        let nuevoMonto=(monto%100);
-    }
-    if(monto>=50){
-        
-    }
-    if(monto>=20){
-        
-    }
-    if(monto>=10){
-        
-    }
+while (monto >= 100){
+  billetes100++;
+  monto -= 100;
+}
+while (monto >=50){
+  billetes50++;
+  monto -= 50;
+}
+while (monto >= 20){
+  billetes20++;
+  monto -= 20;
+}
+while (monto >= 10){
+  billetes10++;
+  monto -= 10;
+}
+
+alert("Billetes de 100: " + billetes100 +"\nBilletes de 50: " + billetes50 +
+  "\nBilletes de 20: " + billetes20 +"\nBilletes de 10: " + billetes10);
+console.log("Billetes de 100: " + billetes100 +"\nBilletes de 50: " + billetes50 +
+  "\nBilletes de 20: " + billetes20 +"\nBilletes de 10: " + billetes10);
+
+  
